7ea563e72f3056b2c43415277204c1ba519b63d3
gfR616 <616gfr@gmail.com>
2023-12-06 19:36:05 +0500

    добил анимацию

--- src/components/carusel.jsx
+++ src/components/carusel.jsx
@@ -20,6 +20,7 @@ const Carusel = () => {
 			opacity: 1,
 			transition: {
 				duration: 1,
+				delay: 0.1,
 			},
 		},
 	}
--- src/components/helloBackground/helloPhrase.jsx
+++ src/components/helloBackground/helloPhrase.jsx
@@ -1,29 +1,42 @@
 import { Box, Center, Text } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import React from 'react'
+import { useInView } from 'react-intersection-observer'
 
 const HelloPhrace = () => {
+	const [ref, inView] = useInView()
+	const variants = {
+		hidden: { opacity: 0, scale: 0.5 },
+		show: {
+			opacity: 1,
+			scale: 1,
+			transition: {
+				duration: 1,
+				delay: 0.6,
+			},
+		},
+	}
 	return (
-		<>
-			<Box
-				p={3}
-				as={motion.div}
-				background={'#01010273'}
-				borderRadius={8}
-				animate={{ x: [-1000, 50, 0] }}
+		<motion.div
+			ref={ref}
+			variants={variants}
+			initial="hidden"
+			animate={'show'}
+			style={{
+				textShadow: '2px 2px 2px #4a4747',
+			}}
+		>
+			<Text
+				fontWeight="bold" // делает текст жирным
+				fontSize="30px" // устанавливает размер шрифта
+				color="#c6c6c6"
 			>
-				<Text
-					fontWeight="bold" // делает текст жирным
-					fontSize="30px" // устанавливает размер шрифта
-					color="#c6c6c6"
-				>
-					<Text textAlign="center">Привет!</Text>
-					Вы на сайте, посвещенном моим кошкам,
-					<br />
-					<Text textAlign="center">покрутите колесико вниз, чтобы увидеть их.</Text>
-				</Text>
-			</Box>
-		</>
+				<Text textAlign="center">Привет!</Text>
+				Вы на сайте, посвещенном моим кошкам,
+				<br />
+				<Text textAlign="center">покрутите колесико вниз, чтобы увидеть их.</Text>
+			</Text>
+		</motion.div>
 	)
 }
 
