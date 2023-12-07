5251a9d048af38b6ef01f69eea8ddcffeb994382
gfR616 <616gfr@gmail.com>
2023-12-07 12:54:39 +0500

    добавил возможность запуска без навигации модалки

--- src/components/modalWindow.jsx
+++ src/components/modalWindow.jsx
@@ -7,14 +7,22 @@ import {
 	ModalContent,
 	ModalOverlay,
 } from '@chakra-ui/react'
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
-const ModalWindow = ({ isOpen, onClose, currentPhoto, onNext, onPrev }) => {
-	console.log(currentPhoto)
+const ModalWindow = ({ isOpen, onClose, currentPhoto, onNext, onPrev, navigation }) => {
+	const [navigationButtons, setNavigationButtons] = useState(navigation)
+	const initialRef = useRef()
 	return (
 		<Box>
-			<Modal isOpen={isOpen} onClose={onClose} size="full" bg="black">
+			<Modal
+				initialFocusRef={initialRef}
+				isOpen={isOpen}
+				onClose={onClose}
+				size="full"
+				bg="black"
+			>
 				<ModalOverlay />
+
 				<ModalContent
 					onClick={onClose}
 					style={{
@@ -25,6 +33,12 @@ const ModalWindow = ({ isOpen, onClose, currentPhoto, onNext, onPrev }) => {
 						backgroundColor: '#000000b7',
 					}}
 				>
+					<div
+						ref={initialRef}
+						tabIndex="-1"
+						aria-hidden="true"
+						style={{ position: 'absolute', top: '-5000px' }}
+					/>
 					<ModalBody
 						style={{
 							display: 'flex',
@@ -32,18 +46,24 @@ const ModalWindow = ({ isOpen, onClose, currentPhoto, onNext, onPrev }) => {
 							alignItems: 'center',
 						}}
 					>
-						<Button
-							onClick={(e) => {
-								e.stopPropagation()
-								onPrev()
-							}}
-							colorScheme="whiteAlpha"
-							bg="#edebeb14"
-							h="100vh"
-							w="25vh"
-						>
-							Назад
-						</Button>
+						{navigationButtons && (
+							<Button
+								onClick={(e) => {
+									e.stopPropagation()
+									onPrev()
+								}}
+								colorScheme="whiteAlpha"
+								bg="#edebeb14"
+								h="100vh"
+								w="25vh"
+								// borderRadius={10}
+								border-top-left-radius={0}
+								border-bottom-right-radius={0}
+								_focus={{ outline: 'none' }}
+							>
+								Назад
+							</Button>
+						)}
 						<img
 							onClick={(e) => {
 								e.stopPropagation()
@@ -52,18 +72,21 @@ const ModalWindow = ({ isOpen, onClose, currentPhoto, onNext, onPrev }) => {
 							alt="Selected"
 							style={{ maxHeight: '100vh', width: 'auto' }}
 						/>
-						<Button
-							onClick={(e) => {
-								e.stopPropagation()
-								onNext()
-							}}
-							colorScheme="whiteAlpha"
-							bg="#edebeb14"
-							h="100vh"
-							w="25vh"
-						>
-							Вперед
-						</Button>
+						{navigationButtons && (
+							<Button
+								onClick={(e) => {
+									e.stopPropagation()
+									onNext()
+								}}
+								colorScheme="whiteAlpha"
+								bg="#edebeb14"
+								h="100vh"
+								w="25vh"
+								borderRadius={0}
+							>
+								Вперед
+							</Button>
+						)}
 					</ModalBody>
 				</ModalContent>
 			</Modal>
